459a31c2db73038ff1faf3336ef534b1de461058
jefshi <jefshi@yeah.net>
2021-07-19 11:20:10 +0800

    [主页]主页追加打包与版本信息

--- app-sample/src/main/java/com/csp/sample/customview/MainActivity.java
+++ app-sample/src/main/java/com/csp/sample/customview/MainActivity.java
@@ -1,14 +1,25 @@
 package com.csp.sample.customview;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * @author csp
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String versionInfo = String.format(getString(R.string.version_info),
+                BuildConfig.ASSEMBLE_TIME,
+                BuildConfig.VERSION_CODE + "",
+                BuildConfig.VERSION_NAME);
+
+        ((TextView) findViewById(R.id.tv_version)).setText(versionInfo);
     }
 }
